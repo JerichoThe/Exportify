3fc5dfed24900ccd73fd91561e1a8fe7da0f2fad
Katarisu <114144185+JerichoThe@users.noreply.github.com>
2024-05-16 10:46:54 +0700

    refactor-footer (#6)

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="text-light py-4" style="background-color: #0A6847;">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <h5>About Exportify</h5>
+            <p>Exportify is a platform dedicated to promoting Indonesia's local economy through exporting local goods
+               globally. As a creation of our nation's youth, Exportify aims to showcase Indonesia's rich cultural
+               heritage and diverse products to the world stage. Join us in our mission to empower local artisans and
+               businesses while contributing to the growth of Indonesia's economy</p>
+            <span class="text-body-secondary">&copy; 2024 Exportify, Inc</span>
+
+         </div>
+         <div class="col-md-6 d-flex justify-content-end">
+            <div>
+               <h5>Connect with Us</h5>
+               <ul class="list-unstyled">
+                  <li><a href="#" class="bi bi-facebook text-decoration-none text-light"> Facebook</a></li>
+                  <li><a href="#" class="bi bi-twitter text-decoration-none text-light"> Twitter</a></li>
+                  <li><a href="#" class="bi bi-instagram text-decoration-none text-light"> Instagram</a></li>
+                  <li><a href="#" class="bi bi-linkedin text-decoration-none text-light"> LinkedIn</a></li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
